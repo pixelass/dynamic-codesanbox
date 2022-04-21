457687d5707e3fe7fb3ed2cf0a070349b80b0acd
Gregor Adams <greg@pixelass.com>
2022-04-21 13:35:27 +0200

    chore: add missing code

---
+++ button-click/button-click.js
@@ -0,0 +1,14 @@
+// this line is only needed for the interactive editor…
+const document = getShadowRoot();
+
+const button1 = document.querySelector(".button-1");
+// Something is not right in the next line:
+const button2 = document.querySelector("button-2");
+
+button1.addEventListener("click", logButton1Click);
+
+// TODO: Add another click event listener for button-2
+
+function logButton1Click() {
+  console.log("Button 1 was clicked!");
+}
